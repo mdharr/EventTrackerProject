d4f157f46ca3c97ff3e8495d14d4514d00649bc4
Michael Harrington <mdharr7@gmail.com>
2023-01-21 07:30:48 +0800

    Create Boot/REST project, add JPA project dependency

---
+++ ArcadeArchives/src/main/java/com/skilldistillery/arcades/ArcadeArchivesApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.arcades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcadeArchivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcadeArchivesApplication.class, args);
+	}
+
+}
